a5c6ad47f9060b8a6aadd6f237abe542f611a2f3
pranav2308 <pranav08@terpmail.umd.edu>
2019-07-24 17:03:54 -0400

    Adding Input link form

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ImageLinkForm.css';
+ 
+
+
+const ImageLinkForm = () => {
+	return(
+	<div className = ''>
+		<p className = 'f3'>
+		 {'Input the Image link in the following text box and see the magic!'}
+		</p>
+		<div className = 'center'>
+			<div className = 'form center pa4 br3 shadow-5'>
+				<input className = 'f4 pa2 w-70 center' type = 'text'/>
+				<button className = 'w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Shoot!</button>
+			</div>
+		</div>
+	</div>
+	);
+}
+
+export default ImageLinkForm;
