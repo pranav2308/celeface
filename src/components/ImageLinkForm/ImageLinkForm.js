c803cdd5b07891885f0e29a7e43049424d7cd887
pranav2308 <pranav08@terpmail.umd.edu>
2019-09-15 13:29:24 -0400

    second button home page structure

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,14 +5,15 @@ import './ImageLinkForm.css';
 
 const ImageLinkForm = ({onSearchChange, onSubmitChange}) => {
 	return(
-	<div className = ''>
+	<div>
 		<p className = 'f3'>
 		 {'Input the Image link in the following text box and see the magic!'}
 		</p>
 		<div className = 'center'>
-			<div className = 'form center pa4 br3 shadow-5'>
-				<input className = 'f4 pa2 w-70 center' type = 'text' onChange = {onSearchChange}/>
-				<button className = 'w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick = {onSubmitChange}>Shoot!</button>
+			<div className = 'form center pa3 br3 shadow-5'>
+				<input className = 'f4 pa1 w-80 center' type = 'text' onChange = {onSearchChange}/>	
+				<button className = 'w-30 grow ma1 f4 link ph1 pv2 dib white bg-light-purple' onClick = {onSubmitChange}>Faces</button>
+				<button className = 'w-30 grow ma1 f4 link ph1 pv2 dib white bg-light-purple'>Celebrity</button>
 			</div>
 		</div>
 	</div>
